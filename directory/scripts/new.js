effc4f1e1949eb828ced766133afecc4f6838871
adilrafi <raf21002@byui.edu>
2022-11-24 22:15:56 +0500

    aaaa

--- directory/scripts/new.js
+++ directory/scripts/new.js
@@ -12,7 +12,7 @@ async function getCompanies(){
   
     // Create elements to add to the document
     let card = document.createElement('section');
-    let h3 = document.createElement('h3');
+    let h1 = document.createElement('h1');
 
     let website = document.createElement('a');
     let portrait = document.createElement('img');
@@ -33,7 +33,8 @@ async function getCompanies(){
   
     // Add/append the section(card) with the h2 element
     card.appendChild(portrait);
-    card.appendChild(h3);
+    card.appendChild(h1);
+
     card.appendChild(website);
 
    
