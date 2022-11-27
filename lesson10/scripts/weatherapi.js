30012ac68ccea50694ba796ac90863d5f6917b2f
adilrafi <raf21002@byui.edu>
2022-11-27 23:24:37 +0500

    aaaa

--- lesson10/scripts/weatherapi.js
+++ lesson10/scripts/weatherapi.js
@@ -1,7 +1,7 @@
 // select HTML elements in the document
 const temperature = document.querySelector('#temperature');
 const description = document.querySelector('#currently');
-const weatherIcon = document.querySelector('#weathericon');
+const weathericon = document.querySelector('#weathericon');
 const caption = document.querySelector('figcaption');
 
 const apiURL = 'https://api.openweathermap.org/data/2.5/q=Fairbanks&units=imperial&APPID=883a34efdea0db6447c3fa6c1a893fa8';
@@ -24,18 +24,19 @@ async function apiFetch(){
     }
     
     }
-    apiFetch()
     
-    function capitalize(string) {
-        return `${string.charAt(0).toUpperCase()}${string.slice(1)}`;
-    }
+apiFetch()
     
-    function displayResults(data) {
-        temperature.textContent = data.main.temp.toFixed(0);
-        let desc = capitalize(data.weather[0].description);
-        description.textContent = desc;
-        let icon = data.weather[0].icon;
-        caption.textContent = `Guttenberg, Iowa Weather: ${desc} icon`;
-        weatherIcon.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-        weatherIcon.alt = `Guttenberg, Iowa Weather:  ${desc} icon`;
-    }
+function capitalize(string) {
+    return `${string.charAt(0).toUpperCase()}${string.slice(1)}`;
+}
+
+function displayResults(data) {
+    temperature.textContent = data.main.temp.toFixed(0);
+    let desc = capitalize(data.weather[0].description);
+    description.textContent = desc;
+    let icon = data.weather[0].icon;
+    caption.textContent = `Guttenberg, Iowa Weather: ${desc} icon`;
+    weatherIcon.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+    weatherIcon.alt = `Guttenberg, Iowa Weather:  ${desc} icon`;
+}
