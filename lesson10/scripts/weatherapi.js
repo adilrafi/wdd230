1ad8fa6e8fbd4d51d46b5b2cb5824935793de2bb
adilrafi <raf21002@byui.edu>
2022-11-27 23:30:45 +0500

    aaa

--- lesson10/scripts/weatherapi.js
+++ lesson10/scripts/weatherapi.js
@@ -1,32 +1,30 @@
-// select HTML elements in the document
 const temperature = document.querySelector('#temperature');
 const description = document.querySelector('#currently');
-const weathericon = document.querySelector('#weathericon');
-const caption = document.querySelector('figcaption');
+const weatherIcon = document.querySelector('#weatherIcon');
+const caption = document.querySelector('#figcaption');
 
-const apiURL = 'https://api.openweathermap.org/data/2.5/q=Fairbanks&units=imperial&APPID=883a34efdea0db6447c3fa6c1a893fa8';
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=guttenberg%2C+iowa&units=imperial&APPID=65d7dd34ded5c3f6bbd922d70f9ae239';
 
 async function apiFetch(){
-    try{
-        const response = await fetch(apiURL);
-        if(response.ok){
-            const data = await response.json();
-            console.log(data);
-            displayResults(data);
-        }
-        else{
-    
-        
-            console.log('Response not OK ${await resonse.text()}');
-        }
-    } catch(error){
-        console.log('Error: ${error.message}');
+try{
+    const response = await fetch(apiURL);
+    if(response.ok){
+        const data = await response.json();
+        console.log(data);
+        displayResults(data);
     }
+    else{
+
     
+        console.log('Response not OK ${await resonse.text()}');
     }
-    
+} catch(error){
+    console.log('Error: ${error.message}');
+}
+
+}
 apiFetch()
-    
+
 function capitalize(string) {
     return `${string.charAt(0).toUpperCase()}${string.slice(1)}`;
 }
@@ -37,6 +35,6 @@ function displayResults(data) {
     description.textContent = desc;
     let icon = data.weather[0].icon;
     caption.textContent = `Guttenberg, Iowa Weather: ${desc} icon`;
-    weathericon.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
-    weathericon.alt = `Guttenberg, Iowa Weather:  ${desc} icon`;
+    weatherIcon.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+    weatherIcon.alt = `Guttenberg, Iowa Weather:  ${desc} icon`;
 }
