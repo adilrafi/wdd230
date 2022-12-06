d6c411fd1533ec7bde4c38abc24af0c52329cb7b
adilrafi <113598840+adilrafi@users.noreply.github.com>
2022-12-06 15:29:33 -0600

    qqqq

--- final/scripts/local.js
+++
@@ -1,9 +0,0 @@
-
-
-function local(){
-    const datefieldUK = document.querySelector("aside"); 
-    const d = new Date();
-    const fulldateUK = new Intl.DateTimeFormat("en-UK", { dateStyle: "full"}).format(d);
-    datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
-  }
-  
